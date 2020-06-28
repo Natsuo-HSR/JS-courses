fb0ab99df6df3de282dc5e03944e2d490dfad8c1
Natsuo <natsuo.ht@gmail.com>
2020-06-28 22:58:24 +0300

    added task3

---
+++ Task3 (getting DOM objects)/js/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// button
+let calculateButton = document.querySelector('#start');
+
+// values
+let budget = document.querySelector('.budget-value');
+let daybudget = document.querySelector('.daybudget-value');
+let level = document.querySelector('.level-value');
+let expenses = document.querySelector('.expenses-value');
+let optionalexpenses = document.querySelector('.optionalexpenses-value');
+let income = document.querySelector('.income-value');
+let monthsavings = document.querySelector('.monthsavings-value');
+let yearsavings = document.querySelector('.yearsavings-value');
+let year = document.querySelector('.year-value');
+let month = document.querySelector('.month-value');
+let day = document.querySelector('.day-value');
+
+// expenses input fields
+let budgetInputs = document.querySelectorAll('.expenses-item');
+
+// oprional expenses inputs fields 2
+let budgetOptInputs = document.querySelectorAll('.optionalexpenses-item');
+
+// others 
+let chooseIncome = document.querySelector(".choose-income");
+let savsingsCheck = document.querySelector('.savings');
+
