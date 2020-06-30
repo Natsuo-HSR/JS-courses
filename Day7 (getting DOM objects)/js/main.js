95688bda4a7326fce2c8363d87ca12247ed5a098
Natsuo <natsuo.ht@gmail.com>
2020-06-30 12:34:30 +0300

    added some changes

--- Day7 (getting DOM objects)/js/main.js
+++ Day7 (getting DOM objects)/js/main.js
@@ -12,17 +12,32 @@ let optionalexpenses = document.querySelector('.optionalexpenses-value');
 let income = document.querySelector('.income-value');
 let monthsavings = document.querySelector('.monthsavings-value');
 let yearsavings = document.querySelector('.yearsavings-value');
+
+// results
 let year = document.querySelector('.year-value');
 let month = document.querySelector('.month-value');
 let day = document.querySelector('.day-value');
 
-// expenses input fields
+// expenses input fields ([n%2==0]-name; [n%2!=0]-cost)
 let budgetInputs = document.querySelectorAll('.expenses-item');
+let budgetBtn = document.querySelector('.expenses-item-btn');
 
-// oprional expenses inputs fields 2
+// oprional expenses inputs fields ([n] - name)
 let budgetOptInputs = document.querySelectorAll('.optionalexpenses-item');
+let budgetOptBtn = document.querySelector('.optionalexpenses-btn');
 
-// others 
+let dailyBudgetBtn = document.querySelector('.count-budget-btn');
+
+
+// possible income filed (names in ,)
 let chooseIncome = document.querySelector(".choose-income");
-let savsingsCheck = document.querySelector('.savings');
+
+// addiction income box
+let savingsCheck = document.querySelector('.savings');
+let savingSum = document.querySelector('.choose-sum');
+let savingPercent = document.querySelector('.choose-percent');
+
+// begin calculations btn
+let beginCalc = documnet.querySelector('.start');
+
 
